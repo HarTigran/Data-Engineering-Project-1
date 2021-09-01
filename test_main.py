1e32b9c8b847b3c0b55d5a9a6b7440a56e260f36
Tigran <tigran.harutyunyan@duke.edu>
2021-09-01 04:30:18 +0000

    added text file

---
+++ test_main.py
@@ -0,0 +1,19 @@
+import requests
+
+def ping():
+    r = requests.get('http://api.open-notify.org/iss-now.json')
+    iss_long = r.json()['iss_position']['longitude']
+    iss_lat = r.json()['iss_position']['latitude']
+    
+    b = requests.get('https://geolocation-db.com/json/')
+    your_long = b.json()['longitude']
+    your_lat = b.json()['latitude']
+    try:
+        float(iss_long) 
+        float(iss_lat)
+        float(your_long)
+        float(your_lat)
+    except ValueError:
+        print("Link is unavailable")
+
+assert ping() != 'Link is unavailable'
